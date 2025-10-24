1fd83985ae5539a1d1d99b3ff698e06d86788d8e
alfinanil <finaaanil3@gmail.com>
2025-10-24 12:18:32 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Java Version Control Practice ===");
+        System.out.println("Program ini dibuat untuk latihan menggunakan Git dan GitHub.");
+        System.out.println("Belajar Git itu seru! Setiap commit adalah langkah menuju kemajuan.");
+        System.out.println("Terima kasih sudah menggunakan program ini!");
+        System.out.println("Aku cinta Indonesia");
+    }
+}
